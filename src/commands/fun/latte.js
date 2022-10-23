2b9c12d22f9d513e86e9a779b971096f0b83affc
AcelyonYT <acelyonyt@gmail.com>
2022-10-23 13:34:02 -0400

    localization
    added localizations to 2 commands

--- src/commands/fun/latte.js
+++ src/commands/fun/latte.js
@@ -4,12 +4,22 @@ const fs = require("fs");
 module.exports = {
     info: {
         name: "latte",
-        description: "random of the cat latte",
+        name_localizations: {
+            de: "milch",
+            "pt-BR": "leite",
+            hu: "tej"
+        },
+        description: "random pictures of the cat latte",
+        description_localizations: {
+            de: "zufällige Bilder vom Katzen-Latte",
+            "pt-BR": "imagens aleatórias do latte gato",
+            hu: "véletlenszerű képek a macska latte-ról"
+        },
         type: ApplicationCommandType.ChatInput
     },
     async execute(app, interaction, data, embed) {
         let files = fs.readdirSync("src/static/lattepictures");
-        let picture = files[Math.floor(Math.random() * files.length)];
+        let picture = files[app.utility.randomInt(0, files.length)];
         await interaction.reply({files: [`./src/static/lattepictures/${picture}`]});
     }
 }
--- src/commands/information/ping.js
+++ src/commands/information/ping.js
@@ -3,20 +3,50 @@ const { ApplicationCommandType, ApplicationCommandOptionType } = require("discor
 module.exports = {
     info: {
         name: "ping",
+        name_localizations: {
+            de: "anpingen",
+            "pt-BR": "ping",
+            hu: "ping"
+        },
         type: ApplicationCommandType.ChatInput,
         description: "send pong or embed",
+        description_localizations: {
+            de: "pong senden oder einbetten",
+            "pt-BR": "enviar pong ou incorporar",
+            hu: "pong küldése vagy beágyazása"
+        },
         options: [{
             name: 'information',
+            name_localizations: {
+                de: "information",
+                "pt-BR": "informação",
+                hu: "információ"
+            },
             description: 'sends information or just pong',
+            description_localizations: {
+                de: "sendet informationen oder einfach nur Pong",
+                "pt-BR": "envia informações ou apenas pong",
+                hu: "információt küld, vagy csak pong"
+            },
             type: ApplicationCommandOptionType.String,
             required: true,
             choices: [
                 {
                     name: "Ping",
+                    name_localizations: {
+                        de: "Anpingen",
+                        "pt-BR": "Ping",
+                        hu: "Ping"
+                    },
                     value: "ping"
                 },
                 {
                     name: "Pong",
+                    name_localizations: {
+                        de: "Stinken",
+                        "pt-BR": "Rio Pong",
+                        hu: "Pong"
+                    },
                     value: "pong"
                 }
             ]
